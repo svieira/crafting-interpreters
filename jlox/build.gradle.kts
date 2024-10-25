62b6afdc0f1738fb6890b4e372404635f2028498
Sean Vieira <vieira.sean@gmail.com>
2024-10-25 00:52:27 -0400

    Enable preview to allow pattern matching in switch

--- jlox/build.gradle.kts
+++ jlox/build.gradle.kts
@@ -18,6 +18,18 @@ tasks.jar {
     manifest.attributes["Main-Class"] = "com.craftinginterpreters.lox.Lox"
 }
 
+tasks.withType<JavaCompile>().configureEach {
+    options.compilerArgs.add("--enable-preview")
+}
+
+tasks.withType<Test>().configureEach {
+    jvmArgs("--enable-preview")
+}
+
+tasks.withType<JavaExec>().configureEach {
+    jvmArgs("--enable-preview")
+}
+
 tasks.test {
     useJUnitPlatform()
 }
--- jlox/run.sh
+++ jlox/run.sh
@@ -1,3 +1,3 @@
 #!/opt/homebrew/bin/bash
 ./gradlew jar
-rlwrap -H jlox-repl-history java -jar build/libs/jlox-1.0-SNAPSHOT.jar
+rlwrap -H jlox-repl-history java --enable-preview -jar build/libs/jlox-1.0-SNAPSHOT.jar
