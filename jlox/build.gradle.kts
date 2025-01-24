47384c1f9122142b1b5f799e6274990e4d629b42
Sean Vieira <vieira.sean@gmail.com>
2025-01-24 01:33:19 -0500

    Upgrade JUnit

--- jlox/build.gradle.kts
+++ jlox/build.gradle.kts
@@ -10,14 +10,14 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.11.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.jar {
     manifest.attributes["Main-Class"] = "com.craftinginterpreters.lox.Lox"
 }
-//
+
 //tasks.withType<JavaCompile>().configureEach {
 //    options.compilerArgs.add("--enable-preview")
 //}
